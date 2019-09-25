383984add79572e4b8f8e0c1a7f76a7453b49e74
19leunam93 <tech.spuur@quickline.ch>
2019-09-25 17:48:21 +0200

    Fix error in the Dashboard
    Warning: count(): Parameter must be an array or an object that implements Countable

--- administrator/views/dashboard/view.html.php
+++ administrator/views/dashboard/view.html.php
@@ -34,7 +34,7 @@ class AttlistViewDashboard extends JViewLegacy
 		//$this->pagination = $this->get('Pagination');
 
 		// Check for errors.
-		if (count($errors = $this->get('Errors')))
+		if (!empty($errors = $this->get('Errors')))
 		{
 			JError::raiseError(500, implode('<br />', $errors));
 
